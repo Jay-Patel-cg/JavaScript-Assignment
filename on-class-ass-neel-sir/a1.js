905329b30431db525bd51f1256721052bca77099
Jay-Patel-cg <jay.patel.a.cg@gmail.com>
2025-12-08 14:52:44 +0530

    New Add

---
+++ on-class-ass-neel-sir/a1.js
@@ -0,0 +1,29 @@
+// var array = [2,5,3,7,8];
+
+// var obj = {
+//     v1: "a",
+//     v2: "b",
+//     v3: array,
+// };
+
+// var array2 = [{v4:"b"},{v5:"t"}];
+
+// var v8 = {v9:"done"}
+
+// var obj2 = {
+//     v6: "name", 
+//     v7: [1,2,v8]
+// }
+
+// console.log(obj2.v7[2].v9);
+
+
+
+
+
+
+
+
+
+
+// c
